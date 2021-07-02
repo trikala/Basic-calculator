42c52b93c79fc12dea73e070c592656422f84c0c
trikala <86710652+trikala@users.noreply.github.com>
2021-07-02 17:45:38 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,33 @@
+Title = "Basic Calculator"
+print(Title)
+
+a= int(input("Enter your first number - " ))
+b= int(input("Enter your second number - "))
+
+#FOR ADDITION
+print(f"addition of two given numbers:{a+b}")
+
+
+
+#FOR SUBSTRACTION
+print(f"substraction of two given number:{a-b}")
+
+ 
+#FOR MULTIPLICATION
+print(f"multiplication of two given number:{a*b}")
+
+
+#FOR DIVISION (FLAOT VALUE)
+print(f"division of two given number:{a/b}")
+
+
+#FOR DIVISION (INT VALUE)
+print(f"division of two given number:{a//b}")
+
+
+#FOR EXPONENT
+print(f" a to the power b:{a**b}")
+
+
+
+
